aacb8044b4a53bb12d2af6638ac153cf5b096770
FetrisyeDP <fetrisyedelp@gmail.com>
2017-03-10 13:27:17 +0800

    Posttest 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,10 @@ Route::get('/', function () {
 Route::get('/public', function () {
     return('Nama saya : Fetrisye Delp Parenden');
 });
+Route::get('pengguna/{pengguna}', function ($pengguna) {
+    return "Hello World dari pengguna $pengguna";
+});
+Route::get('berita/{berita?}', function ($berita = "Laravel 5.2") {
+    return "berita $berita belum di baca";
+});
+
